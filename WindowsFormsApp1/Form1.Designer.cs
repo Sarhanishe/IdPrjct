788967c25e13248e2c45f0524408852060f8519d
Smyslov <smysl0ff@yahoo.com>
2019-03-03 15:37:20 +0300

    1-color prjct

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class CR
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(265, 65);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(351, 445);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox3.Location = new System.Drawing.Point(697, 65);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(351, 445);
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1328, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.openToolStripMenuItem.Text = "open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.saveToolStripMenuItem.Text = "save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 523);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(351, 82);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1109, 118);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // CR
+            // 
+            this.ClientSize = new System.Drawing.Size(1328, 654);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "CR";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox In;
+        private System.Windows.Forms.PictureBox Out;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button Mono;
+        private System.Windows.Forms.MenuStrip Menu;
+        private System.Windows.Forms.ToolStripMenuItem Open;
+        private System.Windows.Forms.ToolStripMenuItem Save;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem save2ToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,398 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Text.RegularExpressions;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace WindowsFormsApp1
+{
+
+    public partial class CR : Form
+    {
+        double[] forest1 = { 0.7, 0.94, 0.36, 0.52 };
+        double[] city2 = { -25.0088, -23, 3300, 839.4985, 933.3491 };
+        double[] river3 = { -2.2365, -1.0706, 29.2263, 65.0149 };
+        public CR()
+        {
+            InitializeComponent();
+        }
+        //Рекурсия
+        public void Identification(double[,] matr, int k)
+        {
+            int k0, k1, k2, k3;
+            int k0c, k1c, k2c, k3c;
+            int p = matr.GetLength(1);
+            int n = 1;
+            while ((k - System.Math.Pow(4, n)) >= 0)
+            {
+                k = k - (int)System.Math.Pow(4, n);
+                n++;
+            }
+
+            int s = (int)System.Math.Pow(2, n);
+            k0 = 4 * s * (k / s) + 2 * (k % s);
+            k1 = 4 * s * (k / s) + 2 * (k % s) + 1;
+            k2 = 4 * s * (k / s) + 2 * (k % s) + 2 * s;
+            k3 = 4 * s * (k / s) + 2 * (k % s) + 2 * s + 1;
+            if (n < 6)
+            {
+                while (n >= 1)
+            {
+                k0 += (int)System.Math.Pow(4, n);
+                k1 += (int)System.Math.Pow(4, n);
+                k2 += (int)System.Math.Pow(4, n);
+                k3 += (int)System.Math.Pow(4, n);
+                n--;
+            }
+
+            k0c = Eq(Kf(matr, k0));
+            k1c = Eq(Kf(matr, k1));
+            k2c = Eq(Kf(matr, k2));
+            k3c = Eq(Kf(matr, k3));
+            
+                if (k0c != 0)
+                    Pnt(matr, k0, k0c);
+                else Identification(matr, k0);
+                if (k1c != 0)
+                    Pnt(matr, k1, k1c);
+                else Identification(matr, k1);
+                if (k2c != 0)
+                    Pnt(matr, k2, k2c);
+                else Identification(matr, k2);
+                if (k3c != 0)
+                    Pnt(matr, k3, k3c);
+                else Identification(matr, k3);
+            }
+            else
+            {
+                while (n >=1)
+                {
+                    k0 += (int)System.Math.Pow(4, n);
+                    k1 += (int)System.Math.Pow(4, n);
+                    k2 += (int)System.Math.Pow(4, n);
+                    k3 += (int)System.Math.Pow(4, n);
+                    n--;
+                }
+
+                k0c = Eq(Kf(matr, k0));
+                k1c = Eq(Kf(matr, k1));
+                k2c = Eq(Kf(matr, k2));
+                k3c = Eq(Kf(matr, k3));
+                Pnt(matr, k0, k0c);
+                Pnt(matr, k1, k1c);
+                Pnt(matr, k2, k2c);
+                Pnt(matr, k3, k3c);
+                return;
+            }
+        }
+        //Начальная
+        public void Identification(double[,] matr)
+        {
+            int k0, k1, k2, k3;
+            int k0c, k1c, k2c, k3c;
+            k0 = 0; k1 = 1; k2 = 2; k3 = 3;
+            textBox1.Text = 1.ToString();
+            k0c = Eq(Kf(matr, k0));
+            k1c = Eq(Kf(matr, k1));
+            k2c = Eq(Kf(matr, k2));
+            k3c = Eq(Kf(matr, k3));
+            if (k0c != 0)
+                Pnt(matr, k0, k0c);
+            else Identification(matr, k0);
+            if (k1c != 0)
+                Pnt(matr, k1, k1c);
+            else Identification(matr, k1);
+            if (k2c != 0)
+                Pnt(matr, k2, k2c);
+            else Identification(matr, k2);
+            if (k3c != 0)
+                Pnt(matr, k3, k3c);
+            else Identification(matr, k3);
+
+            return;
+        }
+        //поиск коэффициентов образа
+        public double[] Kf(double[,] pix, int k)
+        {
+            
+            int p = pix.GetLength(0);
+            int n = 1;
+            
+
+            while ((k - System.Math.Pow(4, n))>=0)
+            {
+                k = k - (int)System.Math.Pow(4, n);
+                n++;
+            }
+            
+            textBox1.Text = n.ToString();
+            int s = (int)System.Math.Pow(2, n);
+            double[,] m = new double[p/s, p/s];
+            for (int i = (k / s) * p / s , ii=0; i < ((k / s) + 1) * p / s; i++, ii++)
+                for (int j = (k % s) * p / s , jj=0; j < ((k % s) + 1) * p / s; j++ , jj++)
+                    m[ii,jj]= pix[i, j] ;
+            double[] arr = new double[2];
+            var mtr = Matrix<double>.Build.DenseOfArray(m);
+            var svd = mtr.Svd(true);//разложение изображения
+            double[] sing = new double[pix.GetLength(0)];
+            for (int i = 1 ; i <  p / s; i++)
+                sing[i] = svd.W[i, i];
+            double sumx = 0, sumx1 = 0, sumy = 0, sumxy = 0;
+            for (int i = 1; i <p/s; i++)
+            {
+                sumx += i;
+                sumy += sing[i];
+                sumx1 += i * i;
+                sumxy += i * sing[i];
+            }
+            arr[0] = ((sing.Length) * sumxy - (sumx * sumy)) / ((sing.Length) * sumx1 - sumx * sumx);
+            arr[1] = (sumy - arr[0] * sumx) / ((sing.Length));
+            
+            if (k == 61 && n == 5)
+            {
+                using (StreamWriter svdfile = new StreamWriter(@"ftest.txt", append: true))
+                {
+                    // svdfile.Write(i);
+                    svdfile.Write("a0 = ");
+                    svdfile.Write(arr[0]);
+                    svdfile.Write(" b0 = ");
+                    svdfile.Write(arr[1]);
+                    svdfile.Write(" n = ");
+                    svdfile.Write(p/s);
+                    svdfile.WriteLine();
+                   /* for (int i = 0; i < p ; i++)
+                    {
+                        for (int j = 0; j < p ; j++)
+                        {
+                            svdfile.Write(" ");
+                            svdfile.Write(pix[i, j]);
+                        }
+                        svdfile.WriteLine();
+                    }*/
+                };
+            }
+            if (k == 1 && n == 5)
+            {
+                using (StreamWriter svdfile = new StreamWriter(@"futest.txt", append: true))
+                {
+                    // svdfile.Write(i);
+                    svdfile.Write("a0 = ");
+                    svdfile.Write(arr[0]);
+                    svdfile.Write(" b0 = ");
+                    svdfile.Write(arr[1]);
+                    svdfile.Write(" n = ");
+                    svdfile.Write(p / s);
+                    svdfile.WriteLine();
+                    /* for (int i = 0; i < p ; i++)
+                     {
+                         for (int j = 0; j < p ; j++)
+                         {
+                             svdfile.Write(" ");
+                             svdfile.Write(pix[i, j]);
+                         }
+                         svdfile.WriteLine();
+                     }*/
+                };
+            }
+            if (k == 2 && n == 5)
+            {
+                using (StreamWriter svdfile = new StreamWriter(@"futest.txt", append: true))
+                {
+                    // svdfile.Write(i);
+                    svdfile.Write("a0 = ");
+                    svdfile.Write(arr[0]);
+                    svdfile.Write(" b0 = ");
+                    svdfile.Write(arr[1]);
+                    svdfile.Write(" n = ");
+                    svdfile.Write(p / s);
+                    svdfile.WriteLine();
+                    /* for (int i = 0; i < p ; i++)
+                     {
+                         for (int j = 0; j < p ; j++)
+                         {
+                             svdfile.Write(" ");
+                             svdfile.Write(pix[i, j]);
+                         }
+                         svdfile.WriteLine();
+                     }*/
+                };
+            }
+
+            return arr;
+        }
+        //Поиск для полного
+        public double[] Kf(double[,] pix)
+        {
+            
+            double[] arr = new double[2];
+            var mtr = Matrix<double>.Build.DenseOfArray(pix);
+            var svd = mtr.Svd(true);//разложение изображения
+            double[] sing = new double[pix.GetLength(0)];
+            for (int i = 1; i <16; i++)
+                sing[i] = svd.W[i, i];
+            double sumx = 0, sumx1 = 0, sumy = 0, sumxy = 0;
+            for (int i = 1; i <16; i++)
+            {
+                sumx += i;
+                sumy += sing[i];
+                sumx1 += i * i;
+                sumxy += i * sing[i];
+            }
+            arr[0] = ((sing.Length) * sumxy - (sumx * sumy)) / ((sing.Length) * sumx1 - sumx * sumx);
+            arr[1] = (sumy - arr[0] * sumx) / ((sing.Length));
+            return arr;
+
+        }
+
+        //Соответствие
+        public int Eq(double[] kf)
+        {
+            int n=0;
+           /* if (kf[0] == 0 && kf[1] == 0)
+                n = 1;*/
+            if ((kf[0] >= forest1[0]) && (kf[0] <= forest1[1]) && (kf[1] >= forest1[2]) && (kf[1] <= forest1[3]))
+                n = 1;
+            else if ((kf[0] >= city2[0]) && (kf[0] <= city2[1]) && (kf[1] >= city2[2]) && (kf[1] <= city2[3]))
+                n=2;
+            else if ((kf[0] >= river3[0]) && (kf[0] <= river3[1]) && (kf[1] >= river3[2]) && (kf[1] <= river3[3]))
+                n=3;
+            return n;
+
+        }
+        //Закрашивание
+        public void Pnt(double[,] pix, int k,int c)
+        {
+            int p = pix.GetLength(1);
+            int n = 1;
+            while ((k - System.Math.Pow(4, n)) >= 0)
+            {
+                k = k - (int)System.Math.Pow(4, n);
+                n++;
+            }
+            int s = (int)System.Math.Pow(2, n);
+            for (int i = (k / s) * p / s; i < ((k / s) + 1) * p / s; i++)
+                for (int j = (k % s) * p / s; j < ((k % s) + 1) * p / s; j++)
+                    pix[i,j] = c;
+
+
+        }
+        //Основная часть
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = (Bitmap)pictureBox2.Image;//создаём объект битмапа для записи в него чёрнобелого изображения
+            int M = bmp.Height;
+            int N = bmp.Width;
+            double[,] a = new double[M, N];
+            for (int y = 0; y < N; y++)
+                for (int x = 0; x < M; x++)
+                {
+                    a[x, y] = bmp.GetPixel(y, x).R;
+                }
+
+           /* double[] koef = new double[2];
+            koef = Kf(a);//получение коэффициентов образа
+
+            using (StreamWriter svdfile = new StreamWriter(@"ftest.txt", append: true))
+            {
+                // svdfile.Write(i);
+                svdfile.Write("a0 = ");
+                svdfile.Write(koef[0]);
+                svdfile.Write(" b0 = ");
+                svdfile.Write("lugkjgjhgjhgmjgmjghjjgjh");
+                svdfile.WriteLine();
+            };*/
+            textBox1.Text = (0 % 2).ToString();
+            Identification(a);
+            textBox1.Text = "end id";
+            Bitmap image1 = new Bitmap(bmp.Height, bmp.Width);
+            Color newColor;
+            // Loop through the images pixels to reset color.
+            for (int x = 0; x < image1.Width; x++)
+            {
+                for (int y = 0; y < image1.Height; y++)
+                {
+                    switch (a[x,y])
+                    {
+                        case 1:
+                             newColor = Color.FromArgb(0, 250, 0);
+                            break;
+                        case 2:
+                             newColor = Color.FromArgb(250, 0, 0);
+                            break;
+                        case 3:
+                             newColor = Color.FromArgb(0, 0,250);
+                            break;
+                        default:
+                            newColor = Color.FromArgb(0, 0, 0);
+                            break;
+                    }
+                    
+                    image1.SetPixel(x, y, newColor);
+                }
+            }
+
+            // Set the PictureBox to display the image.
+            pictureBox3.Image = image1;
+            return;
+
+
+        }
+
+        //сохранение результата работы
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Bitmap bmpSave = (Bitmap)pictureBox3.Image;
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.DefaultExt = "bmp";
+            sfd.Filter = "Image files (*.bmp)|*.bmp|All files (*.*)|*.*";
+            if (sfd.ShowDialog() == DialogResult.OK)
+                bmpSave.Save(sfd.FileName, ImageFormat.Bmp);
+
+        }
+        //обесцвечивание входнового изображения
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Диалог открытия файла.
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap bmp = new Bitmap(ofd.FileName); // // Загружаем изображение.
+                /*Color[,] color = new Color[bmp.Width, bmp.Height];
+                for (int y = 0; y < bmp.Height; y++)
+                    for (int x = 0; x < bmp.Width; x++)
+                    {
+                        color[x, y] = bmp.GetPixel(x, y);
+                    }
+                byte[,] R = new byte[bmp.Size.Width, bmp.Size.Height];
+                byte[,] G = new byte[bmp.Size.Width, bmp.Size.Height];
+                byte[,] B = new byte[bmp.Size.Width, bmp.Size.Height];
+                for (int y = 0; y < bmp.Height; y++)
+                    for (int x = 0; x < bmp.Width; x++)
+                    {
+                        R[x, y] = color[x, y].R;
+                        G[x, y] = color[x, y].G;
+                        B[x, y] = color[x, y].B;
+
+                    }
+                for (int i = 0; i < bmp.Height; i++)
+                    for (int j = 0; j < bmp.Width; j++)
+                    {
+                        bmp.SetPixel(j, i, Color.FromArgb((R[j, i] + G[j, i] + B[j, i]) / 3, (R[j, i] + G[j, i] + B[j, i]) / 3, (R[j, i] + G[j, i] + B[j, i]) / 3));
+                    }*/
+                pictureBox2.Image = bmp; // Устанавливаем в PictureBox/            
+
+            }
+
+        }
+
+    }
+}
